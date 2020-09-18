4b3ce89a8612d582bf3c327fe5354d7e3445520a
Chris DiMaio <chrisdimaio@gmail.com>
2020-09-18 02:01:08 -0400

    Applied checkstyle

--- src/main/java/io/chrisdima/poker/evaluator/Card.java
+++ src/main/java/io/chrisdima/poker/evaluator/Card.java
@@ -4,29 +4,35 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-public class Card implements Comparable<Card>{
-    @Setter
-    private int rank;
-    private final int suit;
-    private final String unicodeChar;
+public class Card implements Comparable<Card> {
+  @Setter
+  private int rank;
+  private final int suit;
+  private final String unicodeChar;
 
-    public Card(int rank, int suit){
-        this.rank = rank;
-        this.suit = suit;
-        this.unicodeChar = Unicode.map(rank,suit);
-    }
+  /**
+   * Instantiate a card object.
+   *
+   * @param rank The rank of the card.
+   * @param suit The suit of the card.
+   */
+  public Card(int rank, int suit) {
+    this.rank = rank;
+    this.suit = suit;
+    this.unicodeChar = Unicode.map(rank, suit);
+  }
 
-    @Override
-    public int compareTo(Card other){
-        if(this.rank != other.rank){
-            return this.rank - other.rank;
-        } else {
-            return this.suit - other.suit;
-        }
+  @Override
+  public int compareTo(Card other) {
+    if (this.rank != other.rank) {
+      return this.rank - other.rank;
+    } else {
+      return this.suit - other.suit;
     }
+  }
 
-    @Override
-    public String toString(){
-        return unicodeChar + "[r: " + rank + " , s: " + suit + "]";
-    }
+  @Override
+  public String toString() {
+    return unicodeChar + "[r: " + rank + " , s: " + suit + "]";
+  }
 }
